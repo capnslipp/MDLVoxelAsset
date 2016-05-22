069796104d39471f5ee5a17a0570e844e60ee165
Slipp Douglas Thompson <git@capnslipp.com>
2016-05-21 23:33:36 -0500

    Eliminated umbrella header `MDLVoxelAsset.h`; rolled it into the same-name class header

--- MDLVoxelAsset/MDLVoxelAsset.h
+++ MDLVoxelAsset/MDLVoxelAsset.h
@@ -4,7 +4,19 @@
 //  Created by Cap'n Slipp on 5/20/16.
 //  Copyright © 2016 Cap'n Slipp. All rights reserved.
 
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MDLVoxelAsset.
+FOUNDATION_EXPORT double MDLVoxelAssetVersionNumber;
+
+//! Project version string for MDLVoxelAsset.
+FOUNDATION_EXPORT const unsigned char MDLVoxelAssetVersionString[];
+
+
+
 #import <ModelIO/ModelIO.h>
+#import "MagicaVoxelVoxData.h"
 
 @class UIColor;
 
