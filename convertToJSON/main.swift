6f4601fc8292e3a229adb500c69125a95029a1c8
Slipp Douglas Thompson <git@capnslipp.com>
2023-03-03 00:20:32 -0600

    Updated convertToJSON to loop over the `voxelAsset.models`

--- convertToJSON/main.swift
+++ convertToJSON/main.swift
@@ -33,52 +33,58 @@ func htmlHexCodeFromColor(_ color:NSColor) -> UInt32 {
 let paletteColors:[NSColor] = voxelAsset.paletteColors
 let paletteHexCodes:[UInt32] = paletteColors.map{ htmlHexCodeFromColor($0) }
 
-let voxelPaletteIndices:[[[UInt8]]] = voxelAsset.voxelPaletteIndices.map{ yzArray in
-	yzArray.map{ zArray in
-		zArray.map { $0.uint8Value }
-	}
-}
 
-let voxelHexCodes:[[[UInt32]]] = voxelPaletteIndices.map{ yzArray in
-	yzArray.map{ zArray in
-		zArray.map { paletteIndex in
-			if paletteIndex == 0 {
-				return 0
-			} else {
-				var hexCode = paletteHexCodes[Array<UInt32>.Index(paletteIndex)]
-				if hexCode == 0x000000 { hexCode = 0x010101 }
-				return hexCode
+func voxelModelToJsonDictEntry(voxelModel:MDLVoxelAssetModel) -> [String:Any]
+{
+	let voxelPaletteIndices:[[[UInt8]]] = voxelModel.voxelPaletteIndices.map{ yzArray in
+		yzArray.map{ zArray in
+			zArray.map { $0.uint8Value }
+		}
+	}
+	
+	let voxelHexCodes:[[[UInt32]]] = voxelPaletteIndices.map{ yzArray in
+		yzArray.map{ zArray in
+			zArray.map { paletteIndex in
+				if paletteIndex == 0 {
+					return 0
+				} else {
+					var hexCode = paletteHexCodes[Array<UInt32>.Index(paletteIndex)]
+					if hexCode == 0x000000 { hexCode = 0x010101 }
+					return hexCode
+				}
 			}
 		}
 	}
-}
-
-let dimensions:(x:Int,y:Int,z:Int) = {
-	let voxelDimensions = voxelAsset.voxelDimensions
-	return ( x: Int(voxelDimensions.x), y: Int(voxelDimensions.y), z: Int(voxelDimensions.z) )
-}()
-
-let voxelHexCodesFlat:[UInt32] = {
-	var flat = [UInt32](repeating: 0x000000, count: Int(dimensions.x * dimensions.y * dimensions.z))
-	var flatI = 0
-	(0..<dimensions.z).forEach{ z in
-		(0..<dimensions.y).forEach{ y in
-			(0..<dimensions.x).forEach{ x in
-				flat[flatI] = voxelHexCodes[x][y][z]
-				flatI += 1
+	
+	let dimensions:(x:Int,y:Int,z:Int) = {
+		let voxelDimensions = voxelModel.voxelDimensions
+		return ( x: Int(voxelDimensions.x), y: Int(voxelDimensions.y), z: Int(voxelDimensions.z) )
+	}()
+	
+	let voxelHexCodesFlat:[UInt32] = {
+		var flat = [UInt32](repeating: 0x000000, count: Int(dimensions.x * dimensions.y * dimensions.z))
+		var flatI = 0
+		(0..<dimensions.z).forEach{ z in
+			(0..<dimensions.y).forEach{ y in
+				(0..<dimensions.x).forEach{ x in
+					flat[flatI] = voxelHexCodes[x][y][z]
+					flatI += 1
+				}
 			}
 		}
-	}
-	return flat
-}()
-
-
-var jsonDict:[String:Any] = {
+		return flat
+	}()
+	
 	return [
 		"voxels": voxelHexCodesFlat,
 		"dims": [ dimensions.x, dimensions.y, dimensions.z ]
 	]
-}()
+}
+
+
+var jsonDict:[String:Any] = [
+	"models": voxelAsset.models.map(voxelModelToJsonDictEntry)
+]
 
 let jsonData = try! JSONSerialization.data(withJSONObject: jsonDict, options: [])
 
